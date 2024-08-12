b036f605de2b8d062e7fc7e2d9c9bf1047399359
OswaldoHB55 <nestor.72607424@gmail.com>
2024-08-12 16:33:59 -0600

    Estructura del proyecto y configuracion del layouts

---
+++ src/main/java/org/esfe/PrestApplication.java
@@ -0,0 +1,13 @@
+package org.esfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrestApplication.class, args);
+	}
+
+}
